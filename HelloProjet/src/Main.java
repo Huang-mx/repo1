6276c1282d076acc75840ae194a25740e1f6861d
Huang Mingxing <1589607555@qq.com>
2020-10-08 21:35:53 +0800

    change again

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("change");
+        System.out.println("change again");
     }
 }
